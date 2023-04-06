aae4b366ffd827f566e02831f5884d1f13e93637
hasan <hasankanj2000@gmail.com>
2023-04-06 15:45:20 +0300

    backend

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const url = process.env.URL;
+
+const db = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default db;
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-///hi
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/db.js";
+import bodyParser from 'body-parser'
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+await db();
+
+const app = new express();
+
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`API IS RUNNING ON PORT: ${port}`);
+});
