b98b3fa38e425a9fb67de7e5e84c897427419b2e
ALI HOMSI <truphleg@gmail.com>
2023-04-09 15:46:03 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./config/db.js";
+import bodyParser from 'body-parser';
 
 import contactRoute from './routes/contactRoute.js';
 
@@ -11,8 +12,9 @@ const app = new express();
 
 await db();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 
 app.use('/api/contact', contactRoute);
